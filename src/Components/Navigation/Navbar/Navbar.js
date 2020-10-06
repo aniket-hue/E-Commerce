dc4d7999754c3fc2690e2bde6ed1c649912ef994
Aniket <freakyyo26@gmail.com>
2020-10-06 22:51:16 +0530

    Navbar bootstrapped added

--- src/Components/Navigation/Navbar/Navbar.js
+++ src/Components/Navigation/Navbar/Navbar.js
@@ -1,38 +1,49 @@
-import React from 'react';
-import classes from './Navbar.module.css';
-import Links from '../../Link/Link';
-import Logo from '../../../assets/crown.svg'
-import { Link } from 'react-router-dom';
-import { auth } from '../../../firebase/firebase.utils';
-import { connect } from 'react-redux';
-import Carticon from '../../Carticon/Carticon';
+import React from "react";
+import classes from "./Navbar.module.css";
+import Links from "../../Link/Link";
+import Logo from "../../../assets/crown.svg";
+import { Link } from "react-router-dom";
+import { auth } from "../../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import Carticon from "../../Carticon/Carticon";
 
 const Navbar = (props) => {
-    const userState = props.currentUser === null ?
-        <Links name='SIGN IN' /> :
-        <p className={classes.signOut} onClick={() => auth.signOut()}>
-            SIGN OUT
-        </p>;
+  const userState =
+    props.currentUser === null ? (
+      <Links name="SIGN IN" />
+    ) : (
+      <p className={classes.signOut} onClick={() => auth.signOut()}>
+        SIGN OUT
+      </p>
+    );
 
-    return (
-        <div className={classes.container}>
-            <div className={classes.logo}>
-                <Link to="/">
-                    <img src={Logo} alt="#" />
-                </Link>
-            </div>
-            <div className={classes.link}>
-                <Links name='SHOP' />
-                <Links name='CONTACT' />
-                {userState}
-            </div>            
-            <Carticon />
+  return (
+    <div className="container" style={{ width: "100vw" }}>
+      <div className="row">
+        <div className="col-md-1">
+          <Link to="/">
+            <img src={Logo} alt="#" />
+          </Link>
         </div>
-    )
-}
+        <div className="col-md-11 d-flex justify-content-end">
+          <div className="col-md-1">
+            <Links name="SHOP" />
+          </div>
+          <div className="col-md-1">
+            <Links name="CONTACT" />
+          </div>
+          <div className="col-md-1">{userState}</div>
+          <div className="col-md-1">
+            <Carticon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProps)(Navbar);
