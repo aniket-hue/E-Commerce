d2cf67d396040cbba65d4bc5f9d4650a43b82d22
Aniket <freakyyo26@gmail.com>
2020-10-06 22:39:28 +0530

    Homepage Bootstrap added

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import classes from './Card.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import classes from "./Card.module.css";
+import { Link } from "react-router-dom";
 
 // 32.2%
-const Card = props => {
-    const style = {
-        backgroundImage: `url(${require(`../../assets/cards/${props.type}.jpg`)})`,
-        flexBasis: `${props.size}%`
-    }
-    return (
-        <div className={classes.wrapper} style={style} >
-            <Link to={`/shop/${props.type}`} className={classes.name}>
-                <p className={classes.type}>{props.type}</p>
-                <p className={classes.shop}>SHOP NOW</p>
-            </Link>
+const Card = (props) => {
+  const style = {
+    backgroundImage: `url(${require(`../../assets/cards/${props.type}.jpg`)})`,
+  };
+  return (
+    <div
+      className={`d-flex justify-content-center align-items-center ${classes.wrapper}`}
+      style={style}
+    >
+      <Link to={`/shop/${props.type}`} className="text-decoration-none">
+        <div className={classes.name}>
+          <div className="pt-2 pt-2 pl-4 pr-4">
+            <p className={classes.type}>{props.type}</p>
+            <p className={classes.shop}>SHOP NOW</p>
+          </div>
         </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
 export default Card;
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import Card from '../../Components/Card/Card'
-import classes from './Homepage.module.css';
+import React from "react";
+import Card from "../../Components/Card/Card";
 
 const Homepage = () => {
-    return (
-        <div>
-            <div className={classes.card}>
-                <Card size={32.2} type='hats' />
-                <Card size={32.2} type='jackets' />
-                <Card size={32.2} type='sneakers' />
-            </div>
-            <div className={classes.card2}>
-                <Card size={49} type='womens' />
-                <Card size={49} type='mens' />
-            </div>
+  return (
+    <div className="container" style={{ maxWidth: "100vw" }}>
+      <div className="row m-md-4 w-100">
+        <div className="col-md-4 m-md-0 m-sm-2">
+          <Card type="hats" />
         </div>
-    );
-}
-export default (Homepage);
+        <div className="col-md-4 m-md-0 m-sm-2">
+          <Card type="jackets" />
+        </div>
+        <div className="col-md-4 m-md-0 m-sm-2">
+          <Card type="sneakers" />
+        </div>
+      </div>
+      <div className="row m-md-4 w-100">
+        <div className="col-md-6 m-md-0 m-sm-2">
+          <Card type="womens" />
+        </div>
+        <div className="col-md-6 m-md-0 m-sm-2">
+          <Card type="mens" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Homepage;
