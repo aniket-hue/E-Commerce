94ea15683d66bda83aaddfde27878e9899031968
Aniket <freakyyo26@gmail.com>
2020-10-06 22:59:11 +0530

    Cart Icon added

--- src/Components/Carticon/Carticon.js
+++ src/Components/Carticon/Carticon.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import Cart from '../../assets/shopping-bag.svg';
-import classes from './Carticon.module.css';
-import { connect } from 'react-redux';
-import { setCartVisiblity } from '../../redux/Cart/cartAction'
+import React from "react";
+import Cart from "../../assets/shopping-bag.svg";
+import classes from "./Carticon.module.css";
+import { connect } from "react-redux";
+import { setCartVisiblity } from "../../redux/Cart/cartAction";
 const Carticon = (props) => {
-    const onCartClick = () => {
-        props.dropdownToggle();
-    }
-    return (
-        <div className={classes.cart}>
-            <p>{props.count}</p>
-            <img onClick={onCartClick} src={Cart} alt='#' />
-        </div>
-    )
-}
+  const onCartClick = () => {
+    props.dropdownToggle();
+  };
+  return (
+    <div className="position-relative w-50">
+      <img onClick={onCartClick} src={Cart} alt="#" />
+      <div className="position-absolute" style={{ top: "20%", left: "40%" }}>
+        <p>{props.count}</p>
+      </div>
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => (
-    { count: state.cart.cartItems.length }
-)
+const mapStateToProps = (state) => ({ count: state.cart.cartItems.length });
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        dropdownToggle: () => (dispatch(setCartVisiblity()))
-    }
-}
+  return {
+    dropdownToggle: () => dispatch(setCartVisiblity()),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Carticon);
