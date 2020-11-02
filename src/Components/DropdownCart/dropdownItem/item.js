bfa34c99dab78abe34d7dbc425c62bf7bed01ab2
Aniket <freakyyo26@gmail.com>
2020-11-02 20:07:57 +0530

    UI fixes and Deployed

--- src/Components/DropdownCart/dropdownItem/item.js
+++ src/Components/DropdownCart/dropdownItem/item.js
@@ -3,12 +3,10 @@ import { connect } from 'react-redux';
 import classes from './item.module.css';
 
 const item = (props) => {
-    console.log(props)
     let itemToAdd = props.cartItems.map(item =>
         (
             <div key={item.id} className={classes.wrapper}>
                 <img className={classes.img} src={item.image} />
-
                 <div className={classes.desc}>
                     <p>{item.name}</p>
                     <p>{item.count} X ${item.price}</p>
@@ -16,15 +14,12 @@ const item = (props) => {
             </div>
         )
     )
-    console.log(itemToAdd)
     return (
-
         <div >
             {itemToAdd}
         </div>
 
-    )        // <div>as</div>
-
+    )       
 }
 
 const mapStateToProps = (state) => ({
