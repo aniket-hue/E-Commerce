bef986cb9a9246317189269e33c76492d7a4ae01
Aniket <freakyyo26@gmail.com>
2020-11-02 18:41:46 +0530

    Unecessary CSS removed

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import Shopitem from '../../Components/Shopitem/Shopitem';
-import classes from './Shop.module.css';
-
+import React from "react";
+import Shopitem from "../../Components/Shopitem/Shopitem";
 
 const Shop = () => {
-    return (
-        <div>
-            <h1 className={classes.h1}>Collections</h1>
-            <Shopitem type="hats" amount={4} />
-            <Shopitem type="sneakers" amount={4} />
-            <Shopitem type="jackets" amount={4} />
-            <Shopitem type="womens" amount={4} />
-            <Shopitem type="mens" amount={4} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1 style={{ fontWeight: "bolder", fontSize: "35px" }}>Collections</h1>
+      <Shopitem type="hats" amount={4} />
+      <Shopitem type="sneakers" amount={4} />
+      <Shopitem type="jackets" amount={4} />
+      <Shopitem type="womens" amount={4} />
+      <Shopitem type="mens" amount={4} />
+    </div>
+  );
+};
 
 export default Shop;
